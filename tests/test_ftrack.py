73196f8b72f52ad04fe67ffa2bd830e058ceaf7f
Nick Ross <nick.ir.ross@gmail.com>
2019-06-25 14:06:56 +0100

    Get tests working

--- tests/test_ftrack.py
+++ tests/test_ftrack.py
@@ -1,7 +1,13 @@
+from urllib.parse import unquote
+
+from flask_security import url_for_security
+
 from tests.base import BaseTest
 
 
 class FTrackTest(BaseTest):
     def test_homepage_unauthenticated(self):
-        response = self.client.get('/')
-        assert response.status_code == 302
+        with self.client as c:
+            response = c.get('/')
+            login_url = url_for_security('login', _external=True) + '?next=/'
+            assert unquote(response.location) == login_url
